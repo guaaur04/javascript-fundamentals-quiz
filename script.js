6d79a6f137e8879204b1a8b0330c0099828ccfef
V Guardado <auricia.guardado@gmail.com>
2020-12-13 04:24:17 -0800

    html/html comments, js TODO outline

---
+++ script.js
@@ -0,0 +1,34 @@
+//Create an html start button 
+
+
+
+
+//Connect the button to the function of starting the timer, and at the same time prompt the user with quiz question
+
+
+
+//When I answer the question, a new question appears. 
+
+
+
+
+
+//If the answer is corrent, timer is unaffected. If the answer is incorrect time is subtracted from the clock. 
+
+
+
+
+//WHEN all questions are answered or the timer reaches 0
+
+
+
+//THEN the game is over
+
+
+
+
+
+//WHEN the game is over
+
+
+//THEN I can save my initials and score
